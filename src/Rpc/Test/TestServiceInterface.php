9673c716c8973bfa30eb9f3dcf52a0f840346845
huanggenghuai <huanggenghuai@gmail.com>
2019-07-11 15:23:36 +0800

    add test

---
+++ src/Rpc/Test/TestServiceInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuxiaodong
+ * Date: 2019/1/21
+ * Time: 21:28
+ */
+
+namespace ServiceComponents\Rpc\Test;
+
+/**
+ * Interface UserServiceInterface
+ * @package ServiceComponents\Rpc\User
+ */
+interface TestServiceInterface
+{
+    /**
+     * 日志
+     * @return mixed
+     */
+    public function log();
+
+
+
+}
